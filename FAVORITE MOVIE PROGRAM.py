fa70cb81b7f212e3def0a6f7562b4e386c20774f
Alfred James III <97080006+AJthree@users.noreply.github.com>
2022-02-12 15:03:32 -0600

    Add files via upload

---
+++ FAVORITE MOVIE PROGRAM.py
@@ -0,0 +1,15 @@
+
+print("This program is about Alfred's favorite movie.")
+
+movieTitle = 'Movie Title: Smokin Aces'
+releaseDate = 'Release Date: 2007'
+movieRating = 'Movie Rating: 6.7/10'
+category1 = 'Category: Action/Crime'
+description1 = 'Description: Las Vegas magician would-be crime boss Buddy -Aces- Israel agrees to testify against former partnerand friend Primo Sparazza in return for admittance into the Witness Protection Programme.However, \
+before the deal can be hammered out, Sparazza orders a hit on Israel, and a host of hit men and women race to kill the snitch and kill the bounty, while the FBI endeavours to keep their source of information alive.'
+
+print(movieTitle)
+print(releaseDate)
+print(movieRating)
+print(category1)
+print(description1)
